36c2e1f555fc190afc6683e8f395354b5ccc32c1
Marjo Lumijärvi <p51731@edu.sasky.fi>
2025-10-06 23:15:48 +0300

    uuden tilin luonti-sivu

--- public/index.php
+++ public/index.php
@@ -23,9 +23,11 @@
     $tapahtuma = haeTapahtuma($_GET['id']);
     if ($tapahtuma) {
       echo $templates->render('tapahtuma',['tapahtuma' => $tapahtuma]);
-    } else {
+      } else {
       echo $templates->render('tapahtumanotfound');
-    }
+      }
+    } else if ($request === '/lisaa_tili') {
+      echo $templates->render('lisaa_tili');
    } else {
     echo $templates->render('notfound');
    }
