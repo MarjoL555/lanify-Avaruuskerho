4915edd4bc4a3cd8537bd5aa8df894193198fa33
Marjo Lumijärvi <p51731@edu.sasky.fi>
2025-12-19 03:46:00 +0200

    kuvagalleria sivulle sisältöä

--- src/view/kuvagalleria.php
+++ src/view/kuvagalleria.php
@@ -1,6 +1,35 @@
 <?php $this->layout('template', ['title' => 'Kuvagalleria']) ?>
 
 <h1>Kuvagalleria</h1>
-<p>, joka näkyy kun klikkaa kuvagalleria navigointi linkkiä</p>
+
 <p> TODO </p>
-<p>Ei sisältöä vielä??</p>
+<p>Ei toimi vielä täydellisesti. Miksi bodyn koko muuttuu ihan vähän?</p>
+
+
+<section id="kuvagalleria">
+      <h2>Valokuvia Avaruuskerhon tapahtumista</h2>
+      <p>Tässä muutamia kerhon tähtiharrastajien ottamia valokuvia. 
+         Osa kuvista on kuvattu kerhon omalla kameralla</p>
+      <div class="kuvaosio">
+        <div>
+          <h3>Vanha Tähtitorni</h3>
+          <img src="images/vanhatorni.jpg" alt="Tähtitornilla">
+          <p>Avaruuskerhon upea Vanha Tähtitorni, jonka tiloissa Konnun-kylän tähtiharrastajat tapaavat.</p>
+        </div>
+        <div>
+          <h3>Konnun järvi</h3>
+          <img src="images/tahtitaivas.jpg" alt="Konnun järvi">
+          <p>Tämä kesäinen tähtitaivas on kuvattu kerhon tähtipäivillä 2025. </p>
+        </div>
+        <div>
+          <h3>Uusi tähtiteleskooppi</h3>
+          <img src="images/teleskooppi.jpg" alt="Teleskooppi">
+          <p>Kerholle hankittiin uusia laitteita kaikkien tähtiharrastajien käyttöön muutama vuosi sitten.</p>
+        </div>
+        <div>
+          <h3>Kerhon syysretki 2024</h3>
+          <img src="images/tahtitorni.jpg" alt="Tähtitornilla">
+          <p>Kävimme porukalla ihailemassa naapurikylän uutta tähtitornia.</p>
+        </div>
+      </div>
+    </section>
