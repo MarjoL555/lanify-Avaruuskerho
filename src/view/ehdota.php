3288e9223d8d3c35e72a30ed8f227816f3a8db1e
Marjo Lumijärvi <p51731@edu.sasky.fi>
2026-01-07 02:19:05 +0200

    Aloittty tapahtuman ehdotus pohja

--- src/view/ehdota.php
+++ src/view/ehdota.php
@@ -6,7 +6,7 @@
    Mitä haluaisit opetella seuraavana kesänä tähtitiede päivillä?
    Tai onko sinulla joku suosikki puhuja, jonka haluaisit kutsua pitämään esitelmää?</p>
 <br>
-<p>Tällä lomakkeella voit ehdottaa omaa ideaa ensi vuoden tapahtumaksi.</p>
+<p>Tällä lomakkeella voit ehdottaa omaa ideaa tai tapahtuma toivetta ensi vuodelle.</p>
 <br>
 <h3> TODO: Tee toiminnallisuus, jolla voi ehdottaa omaa ideaa ja 
     nähdä kaikki muut ehdotetut tapahtumat. 
@@ -14,4 +14,23 @@
     Rakenna tyylillisesti hyvä ja muutenkin toimiva sivusto</h3>
 <br>
 <p>Kesken. Ei sisältöä vielä, ideoi jotain??</p>
+<br>
+
+<div class="ehdotalomake">    
+      <div class="virheteksti"><?= $virhe ?></div>  
+      <form action="" method="POST">
+        <div>
+          <label for="nimi">Nimi:</label>
+          <input type="text" name="nimi" id="nimi" value="<?= $nimi ?>">
+        </div>
+        <div>
+          <label for="toive">Tapahtuma toive tai ehdotus:</label>
+          <input type="text" name="toive" id="toive" value="<?= $toive ?>">
+        </div>
+        <div>        
+          <input type="submit" name="laheta" value="LISÄÄ EHDOTUS">
+        </div>             
+      </form>      
+</div>
+
 </section>
